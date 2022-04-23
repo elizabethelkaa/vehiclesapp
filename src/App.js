7975e016147f534581031367b5268ecb91fc589e
elizabethelkaa <102801196+elizabethelkaa@users.noreply.github.com>
2022-04-23 14:13:58 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header.js';
+import Table from './components/table.js';
+import Footer from './components/footer.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Table />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/datatable.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+
+class dataTable extends Component {
+
+    render() {
+        return (
+            <tr>
+                <td>
+                    {this.props.obj.id}
+                </td>
+                <td>
+                    {this.props.obj.year}
+                </td>
+                <td>
+                    {this.props.obj.make}                    
+                </td>
+                <td>
+                    {this.props.obj.model}
+                </td>
+                <td>
+                    {this.props.obj.type}
+                </td>
+            </tr>
+        );
+    }
+}
+
+export default dataTable;
